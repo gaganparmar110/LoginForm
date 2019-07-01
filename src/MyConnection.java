463b0cf8ef79f8121525c61cd00e9e3fbe9b9a22
gagan <gagan@gagan-PC>
2019-07-01 22:12:05 +0530

    new commit

---
+++ src/MyConnection.java
@@ -0,0 +1,28 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gagan
+ */
+public class MyConnection {
+    
+    public static Connection getConnection(){
+                Connection con=null;
+                try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/registration","root","");
+        } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+        }
+       return con;
+    }
+    
+}
